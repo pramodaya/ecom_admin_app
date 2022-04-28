34f26074a50be82a392c86fc5b9c98a19a7240c9
pramodayaaws <94310356+pramodayaaws@users.noreply.github.com>
2022-04-28 23:05:29 +0530

    duel product in a row added

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -91,7 +91,7 @@ List<Product> demoProducts = [
       Color(0xFFDECB9C),
       Colors.white,
     ],
-    title: "Logitech Head",
+    title: "Logitech Head Omega - Polygon          ",
     price: 20.20,
     description: description,
     rating: 4.1,
--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -6,42 +6,62 @@ import '../../../size_config.dart';
 import 'section_title.dart';
 
 class PopularProducts extends StatelessWidget {
+
+  var p1 = demoProducts;
+  List<Widget> productList = [];
+  List<Widget> rowList = [];
+
+  PopularProducts(){
+    productList = getWidgets(productList);
+
+  
+    rowList = productList.map((element) {
+              return element;
+            }).toList();
+  // productList.map((e) => rowList.add(e));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: SectionTitle(title: "Popular Products", press: () {}),
-        ),
-        SizedBox(height: getProportionateScreenWidth(20)),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Padding(padding: EdgeInsets.all(5),
-          child: Row(
-            children : [
-                            ...List.generate(
-                demoProducts.length,
-                (index) {
-                  if (demoProducts[index].isPopular) {
-                    return ProductCard(product: demoProducts[index]);
-                  }
 
-                  return const SizedBox
-                      .shrink(); // here by default width and height is 0
-                },
-              ),
-              SizedBox(width: getProportionateScreenWidth(20)),
-            ]
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            child: SectionTitle(title: "Popular Products", press: () {}),
           ),
-          )
-        )
-      ],
+          SizedBox(height: getProportionateScreenWidth(20)),
+          ...rowList.map((e) {return e;}
+          ),
+        ],
+      ),
     );
   }
 }
 
+List<Widget> getWidgets(List<Widget> wlist) {
+    var p1 = demoProducts;
+    for (int i = 0; i < p1.length-1; i++) {
+      wlist.add(
+        Container(
+          alignment: FractionalOffset.center,
+          margin: const EdgeInsets.fromLTRB(0, 0, 10, 40.0),
+          child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            ProductCard(product: p1[i]),
+            ProductCard(product: p1[i+1]),
+            
+          ],
+              ),
+        ));
+    }
+    return wlist;
+  }
 
 
 
